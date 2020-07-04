b51afa53c47a4ea496fbc0914fcaafa61c255cfa
DijanaKosovac <kosovacdijana@gmail.com>
2020-07-04 15:06:27 +0200

    Slider done

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 $('.slider').slick({
     infinite: false,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    prevArrow: "<img class='prev' src='./src/images/previous.svg'>",
+    nextArrow: "<img class='next' src='./src/images/next.svg'>"
+
 });
