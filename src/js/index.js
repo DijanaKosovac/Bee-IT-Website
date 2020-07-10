61e1b0f847081e5dee6f47769c3cbb53836629b6
DijanaKosovac <kosovacdijana@gmail.com>
2020-07-10 16:18:21 +0200

    Careers page done and Request a quote modal initialized

--- src/js/index.js
+++ src/js/index.js
@@ -14,3 +14,17 @@ $('.about__slider').slick({
         }
     ]
 });
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementsByClassName("btn")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
